905e76e54629dcb8aa783e255606c74feb029d8e
Myo Thura Ko <myothura.ko@crpusd.org>
2026-02-20 09:19:32 -0800

    did a lot, step 2 done basicallyyyyyy

--- Algorithms.java
+++ Algorithms.java
@@ -1,37 +1,37 @@
 import java.util.ArrayList;
 public class Algorithms {
  // COUNT ALL ZEROS
-  public int countZeros(ArrayList<Integer> arrList){
+  public static int countZeros(ArrayList<Horse> arrList){
     int x = 0;
-for (Integer a : arrList) 
+for (Horse a : arrList) 
 {
-if (a == 0)
+if (a.getWeight() == 0)
    x++;
 }
 return x;
 
   }
 // FIND SMALLEST
-  public int getSmallest(ArrayList<Integer> arrList){
-     int smallest = arrList.get(0);
+  public static int getSmallest(ArrayList<Horse> arrList){
+     int smallest = arrList.get(0).getWeight();
  for (int j = 1; j < arrList.size(); j++)
 {
-    if (arrList.get(j) < smallest)
-         smallest = arrList.get(j);
+    if (arrList.get(j).getWeight() < smallest)
+         smallest = arrList.get(j).getWeight();
 }
 return smallest;
   }
 // GET AVERAGE
-public double getAverage(ArrayList<Double> arrList){
+public static double getAverage(ArrayList<Horse> arrList){
 double sum = 0;
-for (Double a : arrList)
+for (Horse h : arrList)
 {
-    sum += a;
+    sum += h.getWeight();
 }
 return (sum/arrList.size());
 }
 // HAS ZERO
-public boolean hasZero(ArrayList<Boolean> arrList){
+public static boolean hasZero(ArrayList<Boolean> arrList){
   int index = 0;
 boolean found = false;
 while (!found && (index < arrList.size()))
@@ -44,7 +44,7 @@ while (!found && (index < arrList.size()))
 return found;
 }
 //REVERSE LIST
-public ArrayList<Integer> reverseList(ArrayList<Integer> arrList){
+public static ArrayList<Integer> reverseList(ArrayList<Integer> arrList){
 for (int i = 0; i < arrList.size()/2; i++)
 {
    int start = arrList.get(i);
@@ -55,7 +55,7 @@ for (int i = 0; i < arrList.size()/2; i++)
 return arrList;
 }
 // GET SUM
-public int getSum(ArrayList<Integer> arrList){
+public static int getSum(ArrayList<Integer> arrList){
     int sum = 0;
 for (Integer a : arrList)
 {
@@ -64,7 +64,7 @@ for (Integer a : arrList)
 return sum;
 }
 //CHECK DUPLITCATE
-public boolean checkDuplicate(ArrayList<Boolean> arrList){
+public static boolean checkDuplicate(ArrayList<Boolean> arrList){
     for (int i = 0; i < arrList.size(); i++)
 {
    for (int j = i + 1; j < arrList.size(); j++)
@@ -76,7 +76,7 @@ public boolean checkDuplicate(ArrayList<Boolean> arrList){
 return false;
 }
 //SWAP PAIRS
-public ArrayList<Integer> swapPairs(ArrayList<Integer> arrList){
+public static ArrayList<Integer> swapPairs(ArrayList<Integer> arrList){
     for (int i = 0; i < arrList.size()-1; i += 2)
 {
    int a = arrList.get(i);
@@ -86,7 +86,7 @@ public ArrayList<Integer> swapPairs(ArrayList<Integer> arrList){
 return arrList;
 }
 //has all positives
-public boolean hasAllPositive(ArrayList<Integer> arrList){
+public static boolean hasAllPositive(ArrayList<Integer> arrList){
     boolean b = true;
 for (int a : arrList)
 {
@@ -96,7 +96,7 @@ for (int a : arrList)
 return b;
 }
 //move left once, basically rotate
-public ArrayList<Integer> moveLeft(ArrayList<Integer> arrList){
+public static ArrayList<Integer> moveLeft(ArrayList<Integer> arrList){
     int first = arrList.get(0);
     for(int i = 0; i < arrList.size() - 1; i++){
         arrList.set(i, arrList.get(i + 1));
@@ -105,7 +105,7 @@ public ArrayList<Integer> moveLeft(ArrayList<Integer> arrList){
     return arrList;
 }
 //get largest
-public int getLargest(ArrayList<Integer> arrList){
+public static int getLargest(ArrayList<Integer> arrList){
     int n = arrList.get(0);
 
 for (int y = 1; y < arrList.size(); y++)
--- HorseBarnRunner.java
+++ HorseBarnRunner.java
@@ -10,49 +10,22 @@ public class HorseBarnRunner {
     HorseBarn barn = new HorseBarn();
     ArrayList<Horse> barnSpaces;
     barnSpaces = barn.getSpaces();
-   // System.out.println(barnSpaces);
+ 
+    //3.2.5
+    double averageWeight = Algorithms.getAverage(barnSpaces);
+    System.out.println("average weight is " + averageWeight);
 
-    for (Horse h : barnSpaces) {
-      // CODE TO ADD (error because it attempts to change the size of the Array while
-      // traversing)
-      // if (h.getName().equals("Patches"))
-      // barnSpaces.remove(h);
-      System.out.println(h);
+
+
+    
+    
+    for (Horse h : barnSpaces){
+      //Find horses heavier than average
+      if (h.getWeight() > averageWeight){
+        System.out.println("fatter than average: " + h);
+      }
     }
 
-    // int numSpaces = barnSpaces.size();
-    // for (int i = 0; i < numSpaces; i++)
-    // {
-    // Horse h = barnSpaces.get(i);
-    // if (h.getName().equals("Patches"))
-    // {
-    // System.out.println("Bye bye " + barnSpaces.remove(i));
-    // i--;
-    // }
-    // else if (h.getName().equals("Lady"))
-    // {
-    // System.out.println("Bye bye " + barnSpaces.remove(i));
-    // i--;
-    // }
-    // }
-    // int i = 0;
-    // while (i < barnSpaces.size()) {
-    //   Horse h = barnSpaces.get(i);
-
-    //   if (h.getName().equals("Patches")) {
-    //     System.out.println("Bye bye " + barnSpaces.remove(i));
-    //   } else if (h.getName().equals("Lady")) {
-    //     System.out.println("Bye bye " + barnSpaces.remove(i));
-    //   } else {
-    //     i++;
-    //   }
-    // }
-
-
-
-
-    //3.2.5
-    
     
   }
 }
